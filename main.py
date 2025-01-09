9b823f07105244f2081db59daa6c4b14bf58bfcc
woshileow <josh.leow.2010@gmail.com>
2025-01-09 16:15:36 +0800

    update results to Google Sheets

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from google_sheets import update_google_sheet
 from scraper.scraper import *
-from scraper.navigator import *
 from config import BASE_URL, CHROME_DRIVER_PATH
+from scraper.scraper import get_data_from_hyperlink
 
 if __name__ == "__main__":
     # Use the Service class to specify the ChromeDriver path
@@ -11,5 +12,14 @@ if __name__ == "__main__":
     hyperlinks = get_hyperlinks(BASE_URL)
     driver.quit()
 
+    rows_to_update=[] # List of lists to store rows to be added to Google Sheet
     for link in hyperlinks:
-        navigate_to_hyperlink(BASE_URL, link, CHROME_DRIVER_PATH)
+        rows_to_update = get_data_from_hyperlink(rows_to_update, BASE_URL, link, CHROME_DRIVER_PATH) # List of lists to store rows to be added to Google Sheet
+
+    # Update Google Sheets
+    update_google_sheet(
+        sheet_name="CMC_new",
+        data=rows_to_update,
+        credentials_file=os.path.join(os.path.dirname(__file__), "credentials.json"),
+    )
+
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
 from scraper.pages import *
 import os
 
@@ -9,9 +12,7 @@ def get_hyperlink(url):
 
     # Select element using CSS selector
     hyperlink = soup.select_one(FIRST_HYPERLINK)
-
     return hyperlink["href"] if hyperlink else None
-
 
 def get_hyperlinks(base_url):
     script_dir = os.path.dirname(__file__)
@@ -23,7 +24,7 @@ def get_hyperlinks(base_url):
     try:
         with open(abs_file_path, "r") as file:
             last_hyperlink = file.read().strip()
-            print(f"last hyper link is: {last_hyperlink}")
+            print(f"Current link in last_hyperlink.txt is: {last_hyperlink}")
     except:
         print(f"Failed to locate last_hyperlink.txt file in path: [{abs_file_path}]")
     # Fetch the webpage
@@ -51,18 +52,32 @@ def get_hyperlinks(base_url):
         hyperlink = link_tag["href"]
         if i == 1:
             first_hyperlink = hyperlink  # Remember the first hyperlink
-            print(f"first_hyperlink is: {hyperlink}")
 
         if hyperlink == last_hyperlink:
             break  # Stop if the hyperlink matches last_hyperlink.txt
 
-        print(f"Row {i} link is: {hyperlink}")
         hyperlinks.append(hyperlink)
+        print(f"Row {i} link is: {hyperlink}")
 
     # Update the last_hyperlink.txt file with the first hyperlink
     if first_hyperlink:
         with open(abs_file_path, "w") as file:
             file.write(first_hyperlink)
-            print(f"Updated last_hyperlink.txt file with {first_hyperlink}")
+            print(f"Updated link in last_hyperlink.txt to: {first_hyperlink}")
 
     return hyperlinks
+
+
+def get_data_from_hyperlink(rows_to_update, base_url, hyperlink, driver_path):
+    # Use the Service class to specify the ChromeDriver path
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service)
+
+    try:
+        driver.get(base_url[:-4] + hyperlink)
+        print(f"  Navigated to: {driver.current_url}\n    Page Title: {driver.title}")
+        rows_to_update.append([driver.current_url, driver.title])
+    finally:
+        driver.quit()
+
+    return rows_to_update
